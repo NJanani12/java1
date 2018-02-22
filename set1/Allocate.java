b2a07b464f41f6e9a218c31ca1c9e194204600d6
NJanani12 <34670297+NJanani12@users.noreply.github.com>
2018-02-22 10:03:02 +0530

    Create Allocate.java

---
+++ set1/Allocate.java
@@ -0,0 +1,17 @@
+package mandate;
+
+class Allocate {
+	  public static void main(String[] args) {
+	 
+	    try {
+	      long data[] = new long[100]; 
+	    }
+	    catch (Exception e) {
+	      System.out.println(e);
+	    }
+	 
+	    
+	      System.out.println("mandate");
+	    
+	  }
+	}
